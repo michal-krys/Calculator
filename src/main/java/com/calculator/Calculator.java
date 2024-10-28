bf6682b162a3e4581a5524dfff4999e89c25e99c
Michal Kryszewski <m.kryszewski@gmail.com>
2024-10-28 22:24:40 +0100

    -

--- src/main/java/com/calculator/Calculator.java
+++ src/main/java/com/calculator/Calculator.java
@@ -1,4 +1,17 @@
 package com.calculator;
 
 public class Calculator {
+
+    public int add ( int a, int b){
+        return a + b;
+    }
+
+    public int subtract ( int a, int b){
+       return a - b;
+    }
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println(calc.subtract(9, 4));
+        System.out.println(calc.add(7, 6));
+    }
 }
